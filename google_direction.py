46d484532827ca1b8d0c827e14d41fdae8626113
Matthieu Bulté <matthieu.bulte.06@gmail.com>
2015-06-14 00:05:27 +0200

    cleanup google_direction.py (it was already great, but y'know)

--- google_direction.py
+++ google_direction.py
@@ -1,22 +1,23 @@
-__author__ = 'laurentmeyer'
-
-# Install the wrapper by typing "pip install -U googlemaps"
+# Install the wrapper by typing 'pip install -U googlemaps'
 # Open Key: AIzaSyDIfhg1xoQKqy2b0BheWqgMuVyFGgWJIkQ
 
 import googlemaps
 import time
 
-def find_distance_between_two_lats(startLatitude, startLongitude, endLatitude, endLongitude):
+def address_from_geocode(latitude, longitude):
+    return gmaps.reverse_geocode({
+        'lat':latitude,
+        'lng':longitude
+    })[0]['formatted_address']
+
+def find_distance_between_two_lats(start_lat, startlng, end_lat, end_lng):
     gmaps = googlemaps.Client(key='AIzaSyDIfhg1xoQKqy2b0BheWqgMuVyFGgWJIkQ')
-    startaddress = gmaps.reverse_geocode({"lat":startLatitude, "lng":startLongitude})[0]['formatted_address']
-    print(startaddress)
-    endaddress = gmaps.reverse_geocode({"lat":endLatitude, "lng":endLongitude})[0]['formatted_address']
-    print(endaddress)
-    directions = gmaps.directions(startaddress, endaddress, mode="walking")
+
+    start_address = address_from_geocode(start_lat, start_lng)
+    end_address = address_from_geocode(end_lat, end_lng)
+
+    directions = gmaps.directions(start_address, end_address, mode='walking')
     meters = directions[0]['legs'][0]['distance']['value']
-    print meters
     seconds = directions[0]['legs'][0]['duration']['value']
-    print seconds
 
-
-find_distance_between_two_lats(48.127701, 11.666358, 48.123805, 11.649020)
+    return {'meters': meters, 'seconds': seconds}
