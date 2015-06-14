b9b4048a09b0ad8acda4ea51af0d28563fed0022
Matthieu Bulté <matthieu.bulte.06@gmail.com>
2015-06-14 06:30:03 +0200

    fix gmaps not in global

--- google_extended.py
+++ google_extended.py
@@ -4,6 +4,8 @@
 import googlemaps
 import time
 
+gmaps = googlemaps.Client(key='AIzaSyDIfhg1xoQKqy2b0BheWqgMuVyFGgWJIkQ')
+
 def address_from_geocode(latitude, longitude):
     return gmaps.reverse_geocode({
         'lat':latitude,
@@ -11,8 +13,6 @@ def address_from_geocode(latitude, longitude):
     })[0]['formatted_address']
 
 def find_distance(start, end):
-    gmaps = googlemaps.Client(key='AIzaSyDIfhg1xoQKqy2b0BheWqgMuVyFGgWJIkQ')
-
     (start_lat, start_lng) = start
     (end_lat, end_lng)     = end
 
